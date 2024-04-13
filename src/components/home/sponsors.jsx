7e2314f4caf032fe0f0b7a193b514c8c1e5b1c4f
H10UR <100491225+H10UR@users.noreply.github.com>
2024-04-13 23:56:11 +0300

    polishing a little

--- src/components/home/sponsors.jsx
+++ src/components/home/sponsors.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const Sponsors = () => {
   return (
     <section className="sponsors">
-      <h1>Our Beloved Sponsors</h1>
+      <h1>Our Sponsors</h1>
       <div>
         <a href="/contact">Huh... No sponsors yet. But you can contact us and change that!</a>
       </div>
